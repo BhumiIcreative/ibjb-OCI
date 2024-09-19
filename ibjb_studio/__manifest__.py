dd7280730a9ff06bed4b46cb97422fb74388a28c
Rajesh Karangiya <rajesh.aktivsoftware@gmail.com>
2024-09-19 19:38:14 +0530

    [ADD][17.0] IBJB Stdio: Model sale_subscription

---
+++ ibjb_studio/__manifest__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+{
+    "name": "IBJB Studio",
+    "summary": "IBJB Studio",
+    "description": """
+        Convert all studio customization to code""",
+    "author": "Aktic software",
+    "website": "http://www.aktivsoftware.com",
+    "category": "Studio customization",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_subscription",
+        "maintenance",
+    ],
+    "data": [],
+}
